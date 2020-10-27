16071ef7d6a7b1ab036d0b9463fbdd3a999299ef
unknown <milano1605@gmail.com>
2020-10-27 09:44:12 -0300

    [feat]Melhora as animações do dashboard

--- src/components/Views/Dashboard/Dashboard.tsx
+++ src/components/Views/Dashboard/Dashboard.tsx
@@ -5,22 +5,27 @@ import Balance from "./Graphs/Balance/Balance";
 import Budget from "./Graphs/Budget/Budget";
 import Statement from "./Tables/Statement/Statement";
 import Cards from "./Other/Cards/Cards";
+import QueueAnim from "rc-queue-anim";
+
 const { Header, Footer, Content } = Layout;
 
 function Dashboard() {
   return (
     <Layout>
       <Header style={{ color: "white" }}> Here goes the header</Header>
-      <Content>
-        <div style={{ margin: "0 auto", maxWidth: "1440px" }}>
-          <Row style={{ padding: "16px 16px" }} gutter={16}>
-            <Balance />
-            <Statement />
-          </Row>
-          <Row style={{ padding: "16px 16px" }} gutter={16}>
-            <Cards />
-            <Budget />
-          </Row>
+      <Content style={{ margin: 10 }}>
+        <div style={{ margin: "0 auto", maxWidth: "1920px" }}>
+          <QueueAnim
+            interval={500}
+            duration={2000}
+            component={Row}
+            componentProps={{ gutter: [10, 10] }}
+          >
+            <Balance key="z" />
+            <Statement key="zz" />
+            <Cards key="zzz" />
+            <Budget key="zzzz" />
+          </QueueAnim>
         </div>
       </Content>
       <Footer>Fintech Mock @ 2020 by Vitor Milano</Footer>
