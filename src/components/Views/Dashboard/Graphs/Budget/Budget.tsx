c7b83e0e4bcf217c648b104301011c807338ba6c
unknown <milano1605@gmail.com>
2020-10-27 22:40:49 -0300

    [feat]Melhoria no grafico de orçamento

--- src/components/Views/Dashboard/Graphs/Budget/Budget.tsx
+++ src/components/Views/Dashboard/Graphs/Budget/Budget.tsx
@@ -1,8 +1,137 @@
 import React from "react";
 import "./Budget.scss";
 import { Col, Card } from "antd";
-import { VictoryLegend, VictoryPie, VictoryTooltip } from "victory";
+import { ResponsivePie } from "@nivo/pie";
 
+function MyResponsivePie() {
+  const data = [
+    {
+      id: "Alimentação",
+      label: "Alimentação",
+      value: 590,
+      color: "hsl(228, 70%, 50%)",
+    },
+    {
+      id: "Escritório",
+      label: "Escritório",
+      value: 456,
+      color: "hsl(288, 70%, 50%)",
+    },
+    {
+      id: "Material",
+      label: "Material",
+      value: 376,
+      color: "hsl(249, 70%, 50%)",
+    },
+    {
+      id: "Extras",
+      label: "Extras",
+      value: 230,
+      color: "hsl(221, 70%, 50%)",
+    },
+    {
+      id: "Impostos",
+      label: "Impostos",
+      value: 411,
+      color: "hsl(173, 70%, 50%)",
+    },
+  ];
+
+  return (
+    <ResponsivePie
+      data={data}
+      margin={{ top: 15, right: 10, bottom: 10, left: 10 }}
+      innerRadius={0.5}
+      padAngle={5}
+      cornerRadius={3}
+      colors={{ scheme: "nivo" }}
+      radialLabelsSkipAngle={0}
+      radialLabelsTextXOffset={6}
+      radialLabelsTextColor="#333333"
+      radialLabelsLinkOffset={0}
+      radialLabelsLinkDiagonalLength={16}
+      radialLabelsLinkHorizontalLength={24}
+      radialLabelsLinkStrokeWidth={1}
+      radialLabelsLinkColor={{ from: "color" }}
+      slicesLabelsSkipAngle={10}
+      slicesLabelsTextColor="#333333"
+      sortByValue={true}
+      animate={true}
+      motionStiffness={90}
+      motionDamping={15}
+      defs={[
+        {
+          id: "dots",
+          type: "patternDots",
+          background: "inherit",
+          color: "rgba(255, 255, 255, 0.3)",
+          size: 4,
+          padding: 1,
+          stagger: true,
+        },
+        {
+          id: "lines",
+          type: "patternLines",
+          background: "inherit",
+          color: "rgba(255, 255, 255, 0.3)",
+          rotation: -45,
+          lineWidth: 6,
+          spacing: 10,
+        },
+      ]}
+      fill={[
+        {
+          match: {
+            id: "ruby",
+          },
+          id: "dots",
+        },
+        {
+          match: {
+            id: "c",
+          },
+          id: "dots",
+        },
+        {
+          match: {
+            id: "go",
+          },
+          id: "dots",
+        },
+        {
+          match: {
+            id: "python",
+          },
+          id: "dots",
+        },
+        {
+          match: {
+            id: "scala",
+          },
+          id: "lines",
+        },
+        {
+          match: {
+            id: "lisp",
+          },
+          id: "lines",
+        },
+        {
+          match: {
+            id: "elixir",
+          },
+          id: "lines",
+        },
+        {
+          match: {
+            id: "javascript",
+          },
+          id: "lines",
+        },
+      ]}
+    />
+  );
+}
 function Budget() {
   const data = [
     { x: "Alimentação", y: 200 },
@@ -14,79 +143,7 @@ function Budget() {
     <Col span={12}>
       <Card bodyStyle={{ height: 250 }} hoverable key="k21" title="Orçamento">
         <div className="budget__container">
-          <div style={{ width: "300px" }}>
-            <svg width={300} height={200}>
-              <VictoryPie
-                animate={{
-                  onLoad: {
-                    duration: 2500,
-                    before: () => ({ _y: -1200, label: " " }),
-                    after: (datum) => ({ _y: datum._y }),
-                  },
-                }}
-                events={[
-                  {
-                    target: "data",
-                    eventHandlers: {
-                      onMouseEnter: () => {
-                        return [
-                          {
-                            target: "data",
-                            mutation: (props) => {
-                              return { radius: 85 };
-                            },
-                          },
-                        ];
-                      },
-                      onMouseLeave: () => {
-                        return [
-                          {
-                            target: "data",
-                            mutation: (props) => {
-                              return { radius: 80 };
-                            },
-                          },
-                        ];
-                      },
-                    },
-                  },
-                ]}
-                standalone={false}
-                padAngle={5}
-                width={300}
-                height={200}
-                innerRadius={50}
-                padding={{
-                  left: 100,
-                  bottom: 20,
-                  top: 20,
-                }}
-                colorScale="qualitative"
-                data={data}
-                labels={({ datum }) => {
-                  return `${Math.round((datum.y / 615) * 100)}%`;
-                }}
-                labelComponent={
-                  <VictoryTooltip
-                    center={{ x: 200, y: 100 }}
-                    pointerLength={0}
-                    flyoutStyle={{ strokeOpacity: 0, fill: "transparent" }}
-                  />
-                }
-              />
-              <VictoryLegend
-                standalone={false}
-                colorScale="qualitative"
-                x={0}
-                y={10}
-                gutter={20}
-                centerTitle
-                data={data.map((d) => {
-                  return { name: d.x };
-                })}
-              />
-            </svg>
-          </div>
+          <MyResponsivePie />
         </div>
       </Card>
     </Col>
