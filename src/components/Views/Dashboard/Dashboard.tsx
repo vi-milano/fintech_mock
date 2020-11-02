2e06cb77c66da060282daf9afa681f3640b7e4fb
unknown <milano1605@gmail.com>
2020-11-02 00:42:43 -0300

    [feat]Adição de Menu de usuário no Dashboard

--- src/components/Views/Dashboard/Dashboard.tsx
+++ src/components/Views/Dashboard/Dashboard.tsx
@@ -1,6 +1,6 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./Dashboard.scss";
-import { Card, Col, Layout, Row } from "antd";
+import { Button, Card, Col, Dropdown, Layout, Menu, Row } from "antd";
 import Balance from "./Graphs/Balance/Balance";
 import Budget from "./Graphs/Budget/Budget";
 import Statement from "./Tables/Statement/Statement";
@@ -15,6 +15,9 @@ import {
 } from "../../../services/MockService";
 import { gql, useQuery } from "@apollo/client";
 import NumberFormat from "react-number-format";
+import Avatar from "antd/lib/avatar/avatar";
+import { DownOutlined } from "@ant-design/icons";
+import Modal from "antd/lib/modal/Modal";
 
 const { Header, Footer, Content } = Layout;
 
@@ -68,13 +71,111 @@ interface AccountInfo {
   creditAvailable: string;
 }
 
+function AboutModal(props: any) {
+  const { visible, handleOk, handleCancel } = props;
+  return (
+    <>
+      <Modal
+        centered
+        title={<span style={{ fontSize: 24 }}>Dados da empresa</span>}
+        visible={visible}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      >
+        <p>Nome: Estrela Platina Alimentos Ltda</p>
+        <p>CNPJ: 28.433.294/0001-64</p>
+        <p>Inscrição Estadual: 540.472.404.569</p>
+        <p>Data de Abertura: 25/06/2015</p>
+        <p>CEP: 14784-349</p>
+        <p>Endereço: Avenida Ranulfo Prata 997</p>
+      </Modal>
+    </>
+  );
+}
+
 function Dashboard() {
+  const [visible, setVisible] = useState(false);
   const accInfo = useQuery<AccountInfo>(DASH_ACC_INFO);
   const stateInfo = useQuery(SMALL_STATE);
   const cardInfo = useQuery(CARD_INFO);
+  const showModal = () => {
+    setVisible(true);
+  };
+
+  const handleOk = () => {
+    setVisible(false);
+  };
+
+  const handleCancel = () => {
+    setVisible(false);
+  };
+
+  const menu = (
+    <Menu>
+      <Menu.Item key="0">
+        <a
+          href="#"
+          onClick={(e) => {
+            e.preventDefault();
+            showModal();
+          }}
+        >
+          Dados da empresa
+        </a>
+      </Menu.Item>
+      <Menu.Item key="1">
+        <a href="#">Alterar meus dados</a>
+      </Menu.Item>
+      <Menu.Divider />
+      <Menu.Item key="3">Sair</Menu.Item>
+    </Menu>
+  );
 
   return (
     <Layout>
+      <AboutModal
+        showModal={showModal}
+        handleOk={handleOk}
+        handleCancel={handleCancel}
+        visible={visible}
+      />
+      <Header style={{ color: "white", height: 50 }}>
+        <div
+          style={{
+            height: "100%",
+            display: "flex",
+            justifyContent: "flex-end",
+            alignItems: "center",
+          }}
+        >
+          <Avatar
+            style={{ color: "#5f5f5f", backgroundColor: "#f3f3f3" }}
+            size="large"
+            src={
+              "https://i.pinimg.com/originals/1d/2d/68/1d2d68a435530dc9b36c5a7bb930b8fa.jpg"
+            }
+          />
+          <Dropdown overlay={menu} trigger={["click"]}>
+            <div
+              className="ant-dropdown-link"
+              style={{
+                height: "100%",
+                display: "flex",
+                cursor: "pointer",
+                alignItems: "center",
+              }}
+              onClick={(e) => e.preventDefault()}
+            >
+              <span
+                style={{ color: "white", paddingLeft: 10, paddingRight: 5 }}
+              >
+                Vitor Milano
+              </span>
+              <DownOutlined />
+            </div>
+          </Dropdown>
+        </div>
+      </Header>
       <Content style={{ margin: "10px 10px 0 10px" }}>
         <QueueAnim
           interval={300}
